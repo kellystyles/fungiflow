f7b48edee2b1c126e18b81bafbf440ec9e0fb8ae
Kelly Styles <61216119+kellystyles@users.noreply.github.com>
2023-07-24 22:53:27 +1200

    Update slurm_example.sh
    fixed some grammar

--- slurm_example.sh
+++ slurm_example.sh
@@ -7,21 +7,18 @@
 #SBATCH --job-name=fungiflow
 #SBATCH -o /output/path/fungiflow_%A_%a.log
 #SBATCH --mail-type=ALL
-
+#SBATCH --mail-user=user@email.com
 
 """
 This script will run the Fungiflow pipeline
 
 Setup:
  - short reads
-    must be in fq.gz format, with the suffix `_1.fq.gz` for forward reads
-    and `_2.fq.gz` for reverse reads
+    must be in fq.gz format.
  - ONT reads
-    must be uncompressed and in a single fastq file
- - reads must be prefixed with SLURM_ARRAY_TASK_ID value if running in parallel
-    i.e., if using `#SBATCH --array`
+    must be in a single fastq file.
 
-Paramterers:
+Parameters:
  - `-a` is the array value that will be passed to the pipeline, and the files 
     will use this as a prefix. Here I have used the SLURM_ARRAY_TASK_ID as the 
     array value as then its easier to run multiple datasets in parallel. Both
